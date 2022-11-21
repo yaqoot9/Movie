83f3fe845dcc0562f964cbb6b147d2fd7aea9acf
yaqoot9 <yaqootwishahi@gmail.com>
2022-11-20 23:08:47 -0800

    responsive Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,50 +5,57 @@ function Navbar({loginData,logout}) {
   console.log("Navbar",loginData)
   return (
     <>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
 
 
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+    <div class="col-md-3">
+        <button class="navbar-toggler bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          
+          <span></span>
+          <span></span>
+          <span></span>
+          
+        </button>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
 
     {loginData?<><li class="nav-item active">
-      <Link class="navbar-brand"    to="about">About</Link>
+      <Link className="nav-link"    to="about">About</Link>
       </li>
       
-      <li class="nav-item">
-      <Link class="navbar-brand"    to="home">Home</Link>
+      <li className="nav-item">
+      <Link className="nav-link"    to="home">Home</Link>
       </li>
 
-      <li class="nav-item">
-      <Link class="navbar-brand"    to="people">People</Link>
-      </li>
-
-
-      <li class="nav-item">
-      <Link class="navbar-brand"    to="movies">Movies</Link>
+      <li className="nav-item">
+      <Link className="nav-link"    to="people">People</Link>
       </li>
 
 
-      <li class="nav-item">
-      <Link class="navbar-brand"    to="tvshows">Tvshows</Link>
+      <li className="nav-item">
+      <Link className="nav-link"    to="movies">Movies</Link>
+      </li>
+
+
+      <li className="nav-item">
+      <Link className="nav-link"    to="tvshows">Tvshows</Link>
       </li> </>: ''}
       
       </ul>
      <ul  class="navbar-nav ms-auto">
 
      {!loginData? <><li class="nav-item">
-      <Link class="navbar-brand"    to="register">Register</Link>
+      <Link class="nav-link"    to="register">Register</Link>
       </li>
      
       <li class="nav-item">
-      <Link class="navbar-brand"     to="login">Login</Link>
+      <Link class="nav-link"     to="login">Login</Link>
       </li></>:<>
       <li class="nav-item">
-      <Link class="navbar-brand"    to="logout">welcome {loginData.name}</Link>
+      <Link class="nav-link"    to="logout">welcome {loginData.name}</Link>
       </li>
        <li class="nav-item">
-      <Link class="navbar-brand"  onClick={logout}  to="logout">Logout</Link>
+      <Link class="nav-link"  onClick={logout}  to="logout">Logout</Link>
       </li></>}
 
       
@@ -57,6 +64,7 @@ function Navbar({loginData,logout}) {
       </ul>
    
     
+  </div>
   </div>
 </nav>
     
